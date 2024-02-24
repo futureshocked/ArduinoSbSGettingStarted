079bdc06342404637196280f7b9855f4f8c60b57
Peter Dalmaris <peter@futureshock.com.au>
2024-02-25 08:38:25 +1100

    Update _0650_-_PassiveBuzzerSimple.ino
    Fixed wiring instructions.

--- _0650_-_PassiveBuzzerSimple/_0650_-_PassiveBuzzerSimple.ino
+++ _0650_-_PassiveBuzzerSimple/_0650_-_PassiveBuzzerSimple.ino
@@ -22,7 +22,7 @@
  * 
  *  If you have a three pin passive buzzer, connect the pin marked "-"
  *  to GND, the pin marked "S" to Arduino digital pin 8, and the last
- *  pin to Arduino pin 8.
+ *  pin to Arduino pin 5V.
  *  
  *  Created on November 21 2016 by Peter Dalmaris
  * 
